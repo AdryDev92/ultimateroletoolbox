bc73f690fa373f76640494539fa0581e3b54bad6
psycho <tazzmaniac51@gmail.com>
2017-01-17 13:46:55 +0100

    Update app web role toolbox
    version 0.2

---
+++ mechanics.js
@@ -0,0 +1,36 @@
+function diceD4() {
+
+    var numRandom = Math.floor(Math.random()* 4+1);
+    var box = document.getElementById("D4");
+    box.value = numRandom;
+}
+function diceD6() {
+
+    var numRandom2 = Math.floor(Math.random()* 6+1);
+    var box2 = document.getElementById("D6");
+    box2.value = numRandom2;
+}
+function diceD8(){
+
+    var numRandom3 = Math.floor(Math.random()* 8+1);
+    var box3 = document.getElementById("D8");
+    box3.value = numRandom3;
+}
+function diceD10() {
+
+    var numRandom4 = Math.floor(Math.random()* 10+1);
+    var box4 = document.getElementById("D10");
+    box4.value = numRandom4;
+}
+function diceD12() {
+
+    var numRandom5 = Math.floor(Math.random()* 12+1);
+    var box5 = document.getElementById("D12");
+    box5.value = numRandom5;
+}
+function diceD20() {
+
+    var numRandom6 = Math.floor(Math.random()* 20+1);
+    var box6 = document.getElementById("D20");
+    box6.value = numRandom6;
+}
