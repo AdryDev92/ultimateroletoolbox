696383e956d9fcf5da0d6bdbd8ca34ab46e02865
AdryDev92 <tazzmaniac51@gmail.com>
2017-01-19 02:10:24 +0100

    Update mechanics.js
    changed var by let

--- mechanics.js
+++ mechanics.js
@@ -1,36 +1,36 @@
 function diceD4() {
 
-    var numRandom = Math.floor(Math.random()* 4+1);
-    var box = document.getElementById("D4");
+    let numRandom = Math.floor(Math.random()* 4+1);
+    let box = document.getElementById("D4");
     box.value = numRandom;
 }
 function diceD6() {
 
-    var numRandom2 = Math.floor(Math.random()* 6+1);
-    var box2 = document.getElementById("D6");
+    let numRandom2 = Math.floor(Math.random()* 6+1);
+    let box2 = document.getElementById("D6");
     box2.value = numRandom2;
 }
 function diceD8(){
 
-    var numRandom3 = Math.floor(Math.random()* 8+1);
-    var box3 = document.getElementById("D8");
+    let numRandom3 = Math.floor(Math.random()* 8+1);
+    let box3 = document.getElementById("D8");
     box3.value = numRandom3;
 }
 function diceD10() {
 
-    var numRandom4 = Math.floor(Math.random()* 10+1);
-    var box4 = document.getElementById("D10");
+    let numRandom4 = Math.floor(Math.random()* 10+1);
+    let box4 = document.getElementById("D10");
     box4.value = numRandom4;
 }
 function diceD12() {
 
-    var numRandom5 = Math.floor(Math.random()* 12+1);
-    var box5 = document.getElementById("D12");
+    let numRandom5 = Math.floor(Math.random()* 12+1);
+    let box5 = document.getElementById("D12");
     box5.value = numRandom5;
 }
 function diceD20() {
 
-    var numRandom6 = Math.floor(Math.random()* 20+1);
-    var box6 = document.getElementById("D20");
+    let numRandom6 = Math.floor(Math.random()* 20+1);
+    let box6 = document.getElementById("D20");
     box6.value = numRandom6;
 }
