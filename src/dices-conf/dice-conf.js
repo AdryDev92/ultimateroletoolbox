2c5d7187dd833be18bac84de2e806755c9e77c2d
adrydev92 <tazzmaniac51@gmail.com>
2019-03-20 12:15:13 +0100

    dice function parameterized

--- src/dices-conf/dice-conf.js
+++ src/dices-conf/dice-conf.js
@@ -1,36 +1,5 @@
-function diceD4() {
-
-    let numRandom = Math.floor(Math.random()* 4+1);
-    let box = document.getElementById("D4");
+function dice(longitudDado) {
+    let numRandom = Math.floor(Math.random()* longitudDado +1);
+    let box = document.getElementById("D" + longitudDado);
     box.value = numRandom;
-}
-function diceD6() {
-
-    let numRandom2 = Math.floor(Math.random()* 6+1);
-    let box2 = document.getElementById("D6");
-    box2.value = numRandom2;
-}
-function diceD8(){
-
-    let numRandom3 = Math.floor(Math.random()* 8+1);
-    let box3 = document.getElementById("D8");
-    box3.value = numRandom3;
-}
-function diceD10() {
-
-    let numRandom4 = Math.floor(Math.random()* 10+1);
-    let box4 = document.getElementById("D10");
-    box4.value = numRandom4;
-}
-function diceD12() {
-
-    let numRandom5 = Math.floor(Math.random()* 12+1);
-    let box5 = document.getElementById("D12");
-    box5.value = numRandom5;
-}
-function diceD20() {
-
-    let numRandom6 = Math.floor(Math.random()* 20+1);
-    let box6 = document.getElementById("D20");
-    box6.value = numRandom6;
 }
