b33b62a251214092e2f772ca86e2ea75b7ae294d
adrydev92 <tazzmaniac51@gmail.com>
2019-03-20 11:52:06 +0100

    counters' logic now it works

---
+++ src/counter-conf/count-conf.js
@@ -0,0 +1,11 @@
+let count = 0;
+var countEl = document.getElementById('count');
+
+function plus() {
+ count++;
+ countEl.value = count;
+}
+function minus() {
+ count -- ;
+ countEl.value = count;
+}
