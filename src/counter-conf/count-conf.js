b39488fdcd56fc46f769f32ed1742a5e076278fa
AdryDev92 <tazzmaniac51@gmail.com>
2019-03-22 01:36:00 +0100

    Update counter-conf.js
    Now all counters work correctly.

--- src/counter-conf/count-conf.js
+++ src/counter-conf/count-conf.js
@@ -1,12 +1,56 @@
 let count = 0;
-var countEl = document.getElementById('count');
+let count10 = 0;
+let count50 = 0;
+let count100 = 0;
 
-function plus(counter) {
- count++;
- countEl.value = count;
+let countEl = document.getElementById("count");
+let countEl10 = document.getElementById("count10");
+let countEl50 = document.getElementById("count50");
+let countEl100 = document.getElementById("count100");
+
+ function plus(){
+        count++;
+        countEl.value = count;
+    }
+    function minus(){
+      if (count > 0) {
+        count--;
+        countEl.value = count;
+      }  
+    }
+
+function plus10(){
+  count10+=10;
+  countEl10.value = count10;
 }
 
-function minus(counter) {
- count -- ;
- countEl.value = count;
+function minus10(){
+  if(count10 > 0){
+    count10-=10;
+    countEl10.value = count10;
+  }
+}
+
+function plus50(){
+  count50+=50;
+  countEl50.value = count50;
+}
+
+function minus50(){
+  if(count50 > 0){
+    count50-=50;
+    countEl50.value = count50;
+  }
+}
+
+function plus100(){
+  count100+=100;
+  countEl100.value = count100;
+}
+
+function minus100(){
+  if(count100 > 0){
+    count100-=100;
+    countEl100.value = count100;
+  }
 }
