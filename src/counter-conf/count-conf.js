0fa99753a95099ab494310e5c90e9df4e3139ba5
adrydev92 <tazzmaniac51@gmail.com>
2019-11-22 17:41:55 +0100

    counters reset function deleted

--- src/counter-conf/count-conf.js
+++ src/counter-conf/count-conf.js
@@ -1,64 +1,38 @@
-let count = 0;
-let count10 = 0;
-let count50 = 0;
-let count100 = 0;
-
 let countEl = document.getElementById("count");
 let countEl10 = document.getElementById("count10");
 let countEl50 = document.getElementById("count50");
 let countEl100 = document.getElementById("count100");
-let resetButton = document.getElementById("reset);
+
 
  function plus(){
-        count++;
-        countEl.value = count;
-    }
-    function minus(){
-      if (count > 0) {
-        count--;
-        countEl.value = count;
-      }  
-    }
+  countEl++;
+}
+
+function minus(){
+  countEl--;
+}
 
 function plus10(){
   count10+=10;
-  countEl10.value = count10;
 }
 
 function minus10(){
-  if(count10 > 0){
     count10-=10;
-    countEl10.value = count10;
-  }
 }
 
 function plus50(){
   count50+=50;
-  countEl50.value = count50;
 }
 
+
 function minus50(){
-  if(count50 > 0){
-    count50-=50;
-    countEl50.value = count50;
-  }
+  count50-=50;
 }
 
 function plus100(){
   count100+=100;
-  countEl100.value = count100;
 }
 
 function minus100(){
-  if(count100 > 0){
-    count100-=100;
-    countEl100.value = count100;
-  }
-}
-
-function reset(){
- document.getElementById('count').innerHTML = count;
- document.getElementById('count10').innerHTML = count10;
- document.getElementById('count50').innerHTML = count50;
- document.getElementById('count100').innerHTML = count100;
+  count100-=100;
 }
