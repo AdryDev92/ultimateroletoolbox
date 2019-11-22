45daba9eb2d000a5b591d36248e8187c86e39b05
adrydev92 <tazzmaniac51@gmail.com>
2019-11-22 18:03:58 +0100

    added control variable to counters

--- src/counter-conf/count-conf.js
+++ src/counter-conf/count-conf.js
@@ -2,45 +2,46 @@ let countEl = document.getElementById("count");
 let countEl10 = document.getElementById("count10");
 let countEl50 = document.getElementById("count50");
 let countEl100 = document.getElementById("count100");
+let count = 0;
 
 
  function plus(){
-  countEl++;
-  countEl.value = countEl;
+  count++;
+  countEl.value = count;
 }
 
 function minus(){
-  countEl--;
-  countEl.value = countEl;
+  count--;
+  countEl.value = count;
 }
 
 function plus10(){
-  countEl10+=10;
-  countEl.value = countEl10;
+  count+=10;
+  countEl.value = count;
 }
 
 function minus10(){
-  countEl10-=10;
-countEl10.value = countEl10;
+  count-=10;
+countEl10.value = count;
 }
 
 function plus50(){
-  countEl50+=50;
-  countEl50.value = countEl50;
+  count+=50;
+  countEl50.value = count;
 }
 
 
 function minus50(){
-  countEl50-=50;
-countEl50.value = countEl50;
+  count-=50;
+countEl50.value = count;
 }
 
 function plus100(){
-  countEl100+=100;
-  countEl100.value = countEl100;
+  count+=100;
+  countEl100.value = count;
 }
 
 function minus100(){
-  countEl100-=100;
-countEl100.value = countEl100;
+  count-=100;
+countEl100.value = count;
 }
